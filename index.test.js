92e0d9fcfd49086f6f39e0227f88fece0352cb39
Chris <chrisclayhendrik@gmail.com>
2021-12-02 16:25:33 -0600

    Completed activity

--- index.test.js
+++ index.test.js
@@ -12,22 +12,30 @@ describe("Musician Database", () => {
       name: "Prince",
       type: "guitarist",
     });
+      const testMusician2 = await Musician.create({
+        name:'Kurt Cobain',
+        type: 'guitarist',
+    });
     expect(testMusician.name).toBe("Prince");
     expect(testMusician.type).toBe("guitarist");
+    expect(testMusician2.name).toBe("Kurt Cobain");
+    expect(testMusician2.type).toBe("guitarist");
   });
 });
 
 describe("Instrument Database", () => {
-  beforeAll(async () => {
-    await sequelize.sync({ force: true });
-  });
-
   test("can create instruments", async () => {
     const testInstrument = await Instrument.create({
       name: "guitar",
       type: "stringed",
     });
+    const testInstrument2 = await Instrument.create({
+      name: "bass_guitar",
+      type: "stringed",
+    });
     expect(testInstrument.name).toBe("guitar");
     expect(testInstrument.type).toBe("stringed");
+    expect(testInstrument2.name).toBe("bass_guitar");
+    expect(testInstrument2.type).toBe("stringed");
   });
 });
