6b636da5377e0d8e41d29e3d9a3fc3e3f0abf59b
Chris <chrisclayhendrik@gmail.com>
2021-12-07 11:48:39 -0600

    Added route params, updated routes

--- server/server.js
+++ server/server.js
@@ -8,30 +8,45 @@ const app = express();
 const port = 3000;
 
 //points toward folder of static files
-app.use(express.static('../public'));
+app.use(express.static("../public"));
 
 //GET method on /restaurants route returns all restaurants
-app.get('/restaurants', async (req,res) => {
-    //find all instances of the Model Restaurant
-    const allRestaurants = await Restaurant.findAll()
-    //respond with allRestaurants as a json objeect
-    res.json(allRestaurants)
-})
+app.get("/restaurants", async (req, res) => {
+  //find all instances of the Model Restaurant
+  const allRestaurants = await Restaurant.findAll();
+  //respond with allRestaurants as a json objeect
+  res.json(allRestaurants);
+});
 
-app.get('/menus', async (req,res) => {
-    //find all instances of the Model Menu
-    const allMenus = await Menu.findAll()
-    //respond with allMenus as a json objeect
-    res.json(allMenus)
-})
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  res.json(restaurant);
+});
 
-app.get('/items', async (req,res) => {
-    //find all instances of the Model Item
-    const allItems = await Item.findAll()
-    //respond with allItems as a json objeect
-    res.json(allItems)
-})
+app.get("/menus", async (req, res) => {
+  //find all instances of the Model Menu
+  const allMenus = await Menu.findAll();
+  //respond with allMenus as a json objeect
+  res.json(allMenus);
+});
+
+app.get("/menus/:id", async (req, res) => {
+  const menu = await Menu.findByPk(req.params.id);
+  res.json(menu);
+});
+
+app.get("/items", async (req, res) => {
+  //find all instances of the Model Item
+  const allItems = await Item.findAll();
+  //respond with allItems as a json objeect
+  res.json(allItems);
+});
+
+app.get("/items/:id", async (req, res) => {
+  const item = await Item.findByPk(req.params.id);
+  res.json(item);
+});
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-})
+  console.log(`Server listening at http://localhost:${port}`);
+});
--- test/sequelize_index.test.js
+++ test/sequelize_index.test.js
@@ -1,54 +1,82 @@
-const {sequelize} = require('../src/sequelize_index');
-const {Restaurant} = require('../src/restaurant');
-const { Menu } = require('../src/menu');
-const { Item } = require('../src/item');
+const { sequelize } = require("../src/sequelize_index");
+const { Restaurant } = require("../src/restaurant");
+const { Menu } = require("../src/menu");
+const { Item } = require("../src/item");
 
-describe('Restaurant', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
-    })
+describe("Restaurant", () => {
+  /**
+   * Runs the code prior to all tests
+   */
+  beforeAll(async () => {
+    // the 'sync' method will create tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the
+    // test suite is run
+    await sequelize.sync({ force: true });
+  });
 
-    test('can create a restaurant', async () => {
-        const restaurant = await Restaurant.create({ name: 'Ronalds', image: 'http://some.image.url' })
-        expect(restaurant.id).toBe(1)
-    })
+  test("can create a restaurant", async () => {
+    const restaurant = await Restaurant.create({
+      name: "Ronalds",
+      image: "http://some.image.url",
+    });
+    expect(restaurant.id).toBe(1);
+  });
 
-    test('can create a menu', async () => {
-        const restaurant = await Restaurant.create({ name: 'Ronalds', image: 'http://some.image.url' })
-        const menu = await Menu.create({ name: 'Breakfast', restaurantId: restaurant.id })
-        expect(menu.id).toBe(1)
-    })
+  test("can create a menu", async () => {
+    const restaurant = await Restaurant.create({
+      name: "Ronalds",
+      image: "http://some.image.url",
+    });
+    const menu = await Menu.create({
+      name: "Breakfast",
+      restaurantId: restaurant.id,
+    });
+    expect(menu.id).toBe(1);
+  });
 
-    test('can create an item', async () => {
-        const restaurant = await Restaurant.create({ name: 'Ronalds', image: 'http://some.image.url' })
-        const menu = await Menu.create({ name: 'Breakfast', restaurantId: restaurant.id })
-        const item = await Item.create({ name: 'Eggs', price: '$2.00', menuId: menu.id })
-        expect(item.id).toBe(1)
-    })
+  test("can create an item", async () => {
+    const restaurant = await Restaurant.create({
+      name: "Ronalds",
+      image: "http://some.image.url",
+    });
+    const menu = await Menu.create({
+      name: "Breakfast",
+      restaurantId: restaurant.id,
+    });
+    const item = await Item.create({
+      name: "Eggs",
+      price: "$2.00",
+      menuId: menu.id,
+    });
+    expect(item.id).toBe(1);
+  });
 
-    test('restaurants have menus', async () => {
-        const restaurant = await Restaurant.create({name: 'Ronalds', image: 'http://some.image.url'})
-        const menu = await Menu.create({name: 'set 1'});
-        await restaurant.addMenu(menu);
-        const menus = await restaurant.getMenus();
-        expect(menus[0].name).toBe('set 1');
-    })
+  test("restaurants have menus", async () => {
+    const restaurant = await Restaurant.create({
+      name: "Ronalds",
+      image: "http://some.image.url",
+    });
+    const menu = await Menu.create({ name: "set 1" });
+    await restaurant.addMenu(menu);
+    const menus = await restaurant.getMenus();
+    expect(menus[0].name).toBe("set 1");
+  });
 
-    test('menus have items', async () => {
-        const restaurant = await Restaurant.create({name: 'Ronalds', image: 'http://some.image.url'})
-        const menu = await Menu.create({name: 'set 1'});
-        await restaurant.addMenu(menu);
-        const item = await Item.create({name: 'Eggs', price: '$2.00'});
-        await menu.addItem(item);
-        const items = await menu.getItems();
-        expect(items[0].name).toBe('Eggs');
-    })
+  test("menus have items", async () => {
+    const restaurant = await Restaurant.create({
+      name: "Ronalds",
+      image: "http://some.image.url",
+    });
+    const menu = await Menu.create({ name: "set 1" });
+    await restaurant.addMenu(menu);
+    const item = await Item.create({ name: "Eggs", price: "$2.00" });
+    await menu.addItem(item);
+    const items = await menu.getItems();
+    expect(items[0].name).toBe("Eggs");
+  });
 
-})
-    
+//   afterAll(async () => {
+//     await sequelize.sync({ force: true });
+//     sequelize.close();
+//   });
+ });
